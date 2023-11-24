88fc4e52cb9778964375b573b45dae7cfe8587e1
FastMonkey <mdel3089@gmail.com>
2023-11-24 15:22:05 -0800

    Add search function that uses regex

---
+++ search_regex.py
@@ -0,0 +1,31 @@
+# REGEX for searching files
+import re
+
+# User entries
+search_phrase = input('Enter a search term: ')
+search_file = input('Enter the relative path of the file: ')
+
+# State variables
+match_found = False
+match_count = 0
+
+# Open file
+my_find = open(search_file)
+
+# Search for phrase
+for line in my_find:
+    line = line.rstrip()
+    if line.find(search_phrase) >= 0:
+        match_found = True
+        match_count = match_count +1
+
+# Close the file
+my_find.close()
+
+# Print the results
+if match_found and match_count <= 1:
+    print(f"Found {match_count} result for {search_phrase}.")
+elif match_found and match_count > 1:
+    print(f"Found {match_count} results for {search_phrase}.")
+elif not match_found:
+    print('Could not find search value.')
