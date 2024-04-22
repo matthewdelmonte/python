a1ba677134ae3490f324c349cda9b2c460ca2f26
FastMonkey <mdel3089@gmail.com>
2024-04-22 08:57:37 -0700

    Add password generator function

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+import string
+import random
+
+def pword_gen():
+    password_char = string.ascii_letters + string.punctuation + string.digits
+    length = int(input('Enter the length of the password: '))
+    password = ""
+    for i in range(0, length):
+        password = password + random.choice(password_char)
+    print(password)
+
+pword_gen()
