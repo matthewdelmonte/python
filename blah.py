8e6c27cd21452dac6f74389ed45da5ffff0b973d
Matthew Del Monte <mdel3089@gmail.com>
2025-06-23 12:54:11 -0400

    A few practice files

--- blah.py
+++ blah.py
@@ -2,10 +2,13 @@ from faker import Faker
 
 fake = Faker()
 
-print(fake.name())
-print(fake.address())
-print(fake.email())
-print(fake.phone_number())
-print(fake.date_of_birth())
-print(fake.random_number())
-print(fake.random_element(elements=("apple", "banana", "cherry")))
+for _ in range(10):
+    print(fake.name())
+
+# print(fake.name())
+# print(fake.address())
+# print(fake.email())
+# print(fake.phone_number())
+# print(fake.date_of_birth())
+# print(fake.random_number())
+# print(fake.random_element(elements=("apple", "banana", "cherry")))
